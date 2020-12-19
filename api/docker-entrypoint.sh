9d7d1e1495862f1b9dcf0e5eb5b22fcd9c1b10c1
Dana Bennett <danabenett10@gmail.com>
2020-12-19 11:05:34 -0500

    Yeagh

--- api/docker-entrypoint.sh
+++ api/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gunicorn --log-level warning --bind 0.0.0.0:5042 --reuse-port --workers $(nproc) --worker-class uvicorn.workers.UvicornWorker src.main:app
+gunicorn --log-level warning --bind 0.0.0.0:5042 --reuse-port --workers 1 --worker-class uvicorn.workers.UvicornWorker src.main:app
 
 
 
